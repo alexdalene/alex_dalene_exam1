b3b551ab1251f6b487818833ff26f5e0df81c08f
Alex Dalene <aledal51469@stud.noroff.no>
2023-04-29 06:31:45 +0200

    lots of responsive changes and small overhaul

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,16 @@
 const navbarMenu = document.querySelector("#navbar-menu");
 const navbarLinks = document.querySelector(".navbar-links");
 
+let isOpen = false;
+
 navbarMenu.addEventListener("click", () => {
-  navbarLinks.classList.toggle("shown");
+  if (isOpen) {
+    navbarLinks.removeAttribute("isOpen");
+    isOpen = false;
+  } else {
+    navbarLinks.setAttribute("isOpen", true);
+    isOpen = true;
+  }
+
   navbarMenu.classList.toggle("rotate");
 });
