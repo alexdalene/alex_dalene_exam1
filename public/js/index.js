bfdcb5e14ffe14aa71a557566bbbd49d64b16240
Alex Dalene <aledal51469@stud.noroff.no>
2023-04-30 10:42:55 +0200

    added scrollbar to carousel

--- public/js/index.js
+++ public/js/index.js
@@ -6,11 +6,52 @@ let isOpen = false;
 navbarMenu.addEventListener("click", () => {
   if (isOpen) {
     navbarLinks.removeAttribute("isOpen");
+    navbarMenu.style.transform = "rotate(0deg)";
     isOpen = false;
   } else {
     navbarLinks.setAttribute("isOpen", true);
+    navbarMenu.style.transform = "rotate(90deg)";
     isOpen = true;
   }
-
-  navbarMenu.classList.toggle("rotate");
 });
+
+// the container that is scrollable
+const carouselContainer = document.querySelector(".carousel-container");
+
+// scroll buttons
+const scrollButtonLeft = document.querySelector("#scroll-button-left");
+const scrollButtonRight = document.querySelector("#scroll-button-right");
+
+function scrollProgressBar() {
+  // the bar that is going to fill up as the container scrolls
+  const progressFill = document.querySelector(".progress-fill");
+
+  let scrollDistance = carouselContainer.scrollLeft;
+  let progressPercentage =
+    (scrollDistance /
+      (carouselContainer.scrollWidth - document.documentElement.clientWidth)) *
+    100;
+
+  let val = Math.floor(progressPercentage);
+  progressFill.style.width = val + "%";
+
+  if (val < 0) {
+    progressFill.style.width = "0%";
+  }
+
+  // is progress more than 20% -> show the left button
+  if (val > 20) {
+    scrollButtonLeft.style.opacity = 1;
+  } else {
+    scrollButtonLeft.style.opacity = 0;
+  }
+
+  // is progress more than 80% -> hide the right button
+  if (val > 80) {
+    scrollButtonRight.style.opacity = 0;
+  } else {
+    scrollButtonRight.style.opacity = 1;
+  }
+}
+
+carouselContainer.addEventListener("scroll", scrollProgressBar);
