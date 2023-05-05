83916692f1d62f117c2c7c2aff207e8fec38507f
Alex Dalene <aledal51469@stud.noroff.no>
2023-05-05 19:50:32 +0200

    trying again

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -36,47 +36,45 @@ function makeNavbar() {
 
 makeNavbar();
 
-if (document.documentElement.clientWidth < 905) {
-  const navbarMenu = document.querySelector("#navbar-menu");
-  const navbarLinks = document.querySelector(".navbar-links-mobile");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinks = document.querySelector(".navbar-links-mobile");
 
-  let isOpen = false;
+let isOpen = false;
 
-  function openNavbar() {
-    if (isOpen) {
-      navbarLinks.setAttribute("isOpen", false);
-      navbarMenu.style.transform = "rotate(0deg)";
-      isOpen = false;
-    } else {
-      navbarLinks.setAttribute("isOpen", true);
-      navbarMenu.style.transform = "rotate(-90deg)";
-      isOpen = true;
-    }
+function openNavbar() {
+  if (isOpen) {
+    navbarLinks.setAttribute("isOpen", false);
+    navbarMenu.style.transform = "rotate(0deg)";
+    isOpen = false;
+  } else {
+    navbarLinks.setAttribute("isOpen", true);
+    navbarMenu.style.transform = "rotate(-90deg)";
+    isOpen = true;
   }
-
-  navbarMenu.addEventListener("click", openNavbar);
-
-  // the observer
-  const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
-    },
-    observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("link-animation");
-        } else {
-          entry.target.classList.remove("link-animation");
-        }
-      });
-    }, options);
-
-  // the targets
-  const targets = document.querySelectorAll(".link");
-
-  // the observer is observing the targets
-  targets.forEach((target) => {
-    observer.observe(target);
-  });
 }
+
+navbarMenu.addEventListener("click", openNavbar);
+
+// the observer
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  },
+  observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("link-animation");
+      } else {
+        entry.target.classList.remove("link-animation");
+      }
+    });
+  }, options);
+
+// the targets
+const targets = document.querySelectorAll(".link");
+
+// the observer is observing the targets
+targets.forEach((target) => {
+  observer.observe(target);
+});
